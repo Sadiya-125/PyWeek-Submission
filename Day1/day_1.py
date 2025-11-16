bf24621aa0c2fc14c61abcdeb78126c8a456e7cb
Sadiya Maheen Siddiqui <wajid0257@outlook.com>
2025-11-16 16:02:19 +0530

    Added Day-1

---
+++ Day1/day_1.py
@@ -0,0 +1,31 @@
+# Data Types
+# one=-3
+# strng="hello"
+# Switch=False
+
+# print(strng)
+# print(one)
+
+# name=input("press enter")
+# print("hi ",name)
+
+# age=19
+# if age>18:
+#     print("you are elligible to vote")
+# elif age==18:
+#     print("you are 18")
+# else:
+#     print("you are not ellgible to vote")
+
+# for i in range(10):
+#     print(i)
+
+def add(a,b):
+    return a+b
+
+result=add(3,5)
+result2=add(8,9)
+result3=add(4,9)
+print(result)
+print(result2)
+print(result3)
